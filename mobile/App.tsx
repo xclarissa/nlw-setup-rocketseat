f66a6f04f848e69848a177248a5870fd4ba8b117
xclarissa <clarissaxr@gmail.com>
2023-01-20 17:55:19 -0300

    Instalando tailwind e criando o header

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,43 +7,31 @@ import {
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
 
-import Loading from "./src/assets/components/Loading";
+import Loading from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
-
   // prevenir de exibir antes de carregar as fontes
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_800ExtraBold
+    Inter_800ExtraBold,
   });
 
   // se fonte nao estiver disponivel nao vai retornar nada
-  if(!fontsLoaded) {
-    return (
-      <Loading />
-    )
+  if (!fontsLoaded) {
+    return <Loading />;
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>HELLO WOOOORLD</Text>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    <>
+      <Home />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#09090a",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#000",
-    backgroundColor: "#fffb00",
-    fontWeight: "bold",
-  },
-});
---
+++ mobile/src/components/HabitDay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+
+export function HabitDay() {
+  return (
+    <TouchableOpacity className='bg-zinc-900 rounded-lg border-2 m-1 border-zinc-900' />
+    
+  )
+}

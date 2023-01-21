0f2ab99ddea7c7b2e7a587e112f6a5d566ecf26e
xclarissa <clarissaxr@gmail.com>
2023-01-21 10:49:39 -0300

    Criando componente habitday, criação do cabeçalho dos dias da semana e criando placeholder na tela

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,12 +1,50 @@
-import { View } from "react-native";
-import { HabitDay } from "../components/HabitDay";
+import { Text, View, ScrollView } from "react-native";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
+import generateDatesFromYearBeginning from "../utils/generate-dates-from-year-beginning";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const dateFromYearStart = generateDatesFromYearBeginning();
+
+const minimumSummaryDateSize = 18 * 7;
+const amountOfDaysToFill = minimumSummaryDateSize - dateFromYearStart.length;
+
 export function Home() {
-  return(
+  return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
-      <HabitDay />
+      <View className="flex-row mt-6 mb-2 ">
+        {weekDays.map((weekDay, i) => (
+          <Text
+            key={`${weekDay}-${i}`}
+            className="text-zinc-400 text-xl font-bold  text-center mx-1"
+            style={{ width: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+
+      <ScrollView
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {dateFromYearStart.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, i) => (
+              <View
+                key={i}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-900 opacity-40"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
-  )
+  );
 }
